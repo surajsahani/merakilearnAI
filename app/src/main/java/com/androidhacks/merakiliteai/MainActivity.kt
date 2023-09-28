9db4e41ca8b056cab0962e6b6173a76843b489d7
Dhanshri <dhanshri20@navgurukul.org>
2023-09-28 18:33:26 +0530

    PathwayRecyclerview setup

--- app/src/main/java/com/androidhacks/merakiliteai/MainActivity.kt
+++ app/src/main/java/com/androidhacks/merakiliteai/MainActivity.kt
@@ -3,16 +3,20 @@ package com.androidhacks.merakiliteai
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.androidhacks.merakiliteai.adapter.PathwayAdapter
 import com.androidhacks.merakiliteai.databinding.ActivityMainBinding
+import com.androidhacks.merakiliteai.models.Pathway
 import com.androidhacks.merakiliteai.viewmodel.HomeViewModel
 import com.androidhacks.merakiliteai.viewmodel.HomeViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var viewModel: HomeViewModel
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +27,22 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this, HomeViewModelFactory(repository)).get(HomeViewModel::class.java)
 
         viewModel.pathwayContainer.observe(this) {
-//            initPathwayRecyclerView(it.pathways)
+            initPathwayRecyclerView(it.pathways)
             Log.d("MainActivity", "onCreate: ${it.pathways}")
         }
 
-
     }
+
+    fun initPathwayRecyclerView(pathway: List<Pathway>) {
+        val layoutManager = LinearLayoutManager(this)
+        binding.pathwayRecycler.layoutManager = layoutManager
+
+        val adapter = PathwayAdapter(pathway) {
+            Toast.makeText(this, "CLicked on ${it.name}", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.pathwayRecycler.adapter = adapter
+    }
+
 
 }
---
+++ app/src/main/java/com/androidhacks/merakiliteai/adapter/PathwayAdapter.kt
@@ -0,0 +1,38 @@
+package com.androidhacks.merakiliteai.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.androidhacks.merakiliteai.databinding.ItemPathwayBinding
+import com.androidhacks.merakiliteai.models.Pathway
+import com.bumptech.glide.Glide
+
+class PathwayAdapter (private val pathways : List<Pathway>, private val onPathwayClick : (Pathway) -> Unit) : RecyclerView.Adapter<PathwayAdapter.PathwayViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PathwayViewHolder {
+        val binding = ItemPathwayBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PathwayViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PathwayViewHolder, position: Int) {
+        holder.bind(pathways[position])
+    }
+
+    override fun getItemCount(): Int {
+        return pathways.size
+    }
+
+    inner class PathwayViewHolder(private val binding: ItemPathwayBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(pathway: Pathway) {
+            binding.pathwayText.text = pathway.name
+            Glide.with(binding.logo.context)
+                .load(pathway.logo)
+                .into(binding.logo)
+
+
+            binding.root.setOnClickListener {
+                onPathwayClick(pathway)
+            }
+        }
+    }
+}
